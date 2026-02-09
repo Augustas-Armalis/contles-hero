fe121a13ec1f133ed85f45b68691bf6b8ed4598a
Ares <119808410+Augustas-Armalis@users.noreply.github.com>
2026-02-09 20:40:24 +0200

    Auto-deploy: Mon Feb  9 20:40:24 EET 2026

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 
 function App() {
+  const location = useLocation()
+
+  useEffect(() => {
+    const gtag = window?.gtag
+    if (typeof gtag !== 'function') return
+
+    const page_path = `${location.pathname}${location.search}${location.hash}`
+    gtag('event', 'page_view', {
+      page_path,
+      page_title: document.title,
+    })
+  }, [location.pathname, location.search, location.hash])
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
